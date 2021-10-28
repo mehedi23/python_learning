fe6c8961029448adf519ca01b8906b4dee14b5c4
name <satusatusatu0@gmail.com>
2021-10-28 22:05:40 +0600

    type conversion

---
+++ type-conversion.py
@@ -0,0 +1,20 @@
+# int()
+# str()
+
+# birthday = input('input your birthday ')
+# age = 2020 - int(birthday)
+# print(age)
+
+
+
+# float()
+
+first = input('First: ')
+second = input('Second: ')
+sum = float(first) + float(second)
+print('Sum' + sum)
+
+
+
+
+# bool()
