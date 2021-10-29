a773b7a3dd71b9cca91ab2282837d184646cbd6e
name <satusatusatu0@gmail.com>
2021-10-29 12:44:26 +0600

    oparetor

--- arithmetic.py
+++ arithmetic.py
@@ -8,3 +8,27 @@
 # x += 5
 # x*= 3
 # print(x)
+
+# operator
+# x = 10 + 3 * 2
+# y = (10 + 3) * 2
+# z = 10 + (3 * 2)
+
+# print( 'x =' , x )
+# print( 'y =' , y )
+# print( 'z =' , z )
+
+# comparison
+# all oparetor > , >= , < , <= , == , !=
+# x = 3 < 2
+# x = 3 > 2
+# print(x)
+
+
+# logical operators
+# all operator = and , or, not
+
+price = 25
+log_1 = price > 10 and price != 30
+log_2 = price > 100 or price < 30
+print(log_2)
