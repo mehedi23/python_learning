7b304ab4f585859563f979c42e1737dac09cab88
name <satusatusatu0@gmail.com>
2021-10-29 18:58:33 +0600

    list

---
+++ lists.py
@@ -0,0 +1,16 @@
+name = ["apple" , "tree" , "clock", "pc", "monitor", "car"]
+name[0] = "banana"
+# name.insert(0 , "headphone")
+# name.remove("tree")
+# name.clear()
+
+# print(name[2])
+# print(name[-2])
+# print(name)
+
+# print(name[0:3])
+
+# in opatretor
+chcek = "banana" in name
+print(chcek)
+print(len(name))
