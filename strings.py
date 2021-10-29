09ef0ed5386e53cfd214f14ddce37a9fab85373a
name <satusatusatu0@gmail.com>
2021-10-29 12:30:30 +0600

    strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+course = "Python for beginners"
+scan = course.find('nn')
+print("find=" , scan) # 15
+
+replace = course.replace('for' , 'to')
+print('replace= ' ,  replace) #Python to beginners
+
+
+check = 'Python' in course
+print("check = ", check) # True
