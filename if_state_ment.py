d823fc0d169e2ae7e375e8f91750c9986cc182f4
name <satusatusatu0@gmail.com>
2021-10-29 18:17:22 +0600

    if state ment

---
+++ if_state_ment.py
@@ -0,0 +1,13 @@
+temperature = 25
+
+# if temperature > 30:
+#     print("it's very hot day")
+# else:
+#     print("it is cold")
+
+if temperature > 30:
+    print("it's very hot day")
+elif temperature > 20:
+    print("It's nice day")
+else:
+    print("it is cold")
