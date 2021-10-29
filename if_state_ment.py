e3b9408e20fac46b978dcf7f589be2fc337caddc
name <satusatusatu0@gmail.com>
2021-10-29 18:29:23 +0600

    exercise

--- if_state_ment.py
+++ if_state_ment.py
@@ -5,9 +5,21 @@ temperature = 25
 # else:
 #     print("it is cold")
 
-if temperature > 30:
-    print("it's very hot day")
-elif temperature > 20:
-    print("It's nice day")
+# if temperature > 30:
+#     print("it's very hot day")
+# elif temperature > 20:
+#     print("It's nice day")
+# else:
+#     print("it is cold")
+
+# exercise
+
+weight = input("Weight: ")
+get_extention = input("(K)g or (L)bs: ").upper()
+
+if get_extention == "K":
+    print("Weight in Kg: ", weight)
+elif get_extention == "L":
+    print("Weight in Liter", weight)
 else:
-    print("it is cold")
+    print("Your unit is not correct")
